833597f30cad7f0d07c7272803cc933ae1437b9a
khanhncd2008 <132860339+khanhncd2008@users.noreply.github.com>
2024-06-26 19:53:17 +0700

    apiorderproduct

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,135 +1,134 @@
-import React, { useEffect, useState } from 'react';
-import './Profile.css';
-import { jwtDecode } from 'jwt-decode';
-import { toast, ToastContainer } from 'react-toastify';
+import React, { useEffect, useState } from "react";
+import "./Profile.css";
+import { jwtDecode } from "jwt-decode";
+import { toast, ToastContainer } from "react-toastify";
 import axios from "../../utils/axios.js";
 import coin from "../Assets/coin.png";
 import Header from "../Header/Header";
 
 export const Profile = () => {
-    const [userInfo, setUserInfo] = useState([]);
-    const [userBl, setUserBl] = useState([]);
-    const [data, setData] = useState([]);
-    const [username, setUsername] = useState([]);
+  const [userInfo, setUserInfo] = useState([]);
+  const [userBl, setUserBl] = useState([]);
+  const [data, setData] = useState([]);
+  const [username, setUsername] = useState([]);
 
-   
-    useEffect(() => {
-      const GetData = async () => {
-        try {
-          const result = await axios.get("/Product/getProductByUserId");
-          setData(result.data.value);
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
-      };
-      GetData();
-    }, []);
-
-    useEffect(() => {
-        try {
-          const token = localStorage.getItem('accessToken');
-          if (token) {
-            const decoded = jwtDecode(token);
-            //console.log(decoded);
-            setUserInfo(decoded);
-          } else {
-            toast.error('Bạn chưa đăng nhập');
-            window.location.href = 'http://localhost:3000/homepage';
-          }
-        } catch (err) {
-            toast.error('Bạn chưa đăng nhập');
-            window.location.href = 'http://localhost:3000/homepage';
-        }
-      }, []);
-
-      useEffect(() => {
-        const GetUserBl = async () => {
-            try {
-              const userId = userInfo.nameid;
-              console.log(userId);
-              const result = await axios.get(`http://localhost:5059/api/Account/user/${userId}`);
-              console.log(result);
-              setUserBl(result.data.value.userBalance.balance);
-              setUsername(result.data.value);
-              console.log(username.thumbnail);
-            } catch (error) {
-              console.error("Error fetching data:", error);
-            }
-          };
-          GetUserBl();
-    }, [userInfo])
-
-      const getInitial = (email) => {
-        return email ? email.charAt(0).toUpperCase() : '';
+  useEffect(() => {
+    const GetData = async () => {
+      try {
+        const result = await axios.get("/Product/getProductByUserId");
+        setData(result.data.value);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
+    GetData();
+  }, []);
 
-    const getFirst = (email) => {
-        if (email) {
-            return email.split('@')[0];
-        }
-        return '';
+  useEffect(() => {
+    try {
+      const token = localStorage.getItem("accessToken");
+      if (token) {
+        const decoded = jwtDecode(token);
+        //console.log(decoded);
+        setUserInfo(decoded);
+      } else {
+        toast.error("Bạn chưa đăng nhập");
+        window.location.href = "http://localhost:3000/homepage";
+      }
+    } catch (err) {
+      toast.error("Bạn chưa đăng nhập");
+      window.location.href = "http://localhost:3000/homepage";
+    }
+  }, []);
+
+  useEffect(() => {
+    const GetUserBl = async () => {
+      try {
+        const userId = userInfo.nameid;
+        console.log(userId);
+        const result = await axios.get(
+          `http://localhost:5059/api/Account/user/${userId}`
+        );
+        console.log(result);
+        setUserBl(result.data.value.userBalance.balance);
+        setUsername(result.data.value);
+        console.log(username.thumbnail);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
+    GetUserBl();
+  }, [userInfo]);
+
+  const getInitial = (email) => {
+    return email ? email.charAt(0).toUpperCase() : "";
+  };
+
+  const getFirst = (email) => {
+    if (email) {
+      return email.split("@")[0];
+    }
+    return "";
+  };
 
   return (
     <div className="profile-container">
-      <Header/>
+      <Header />
       <main className="profile-content">
         <div className="profile-header">
-          <div className="profile-initials"><img src={username.thumbnail} alt="Product Thumbnail" /></div>
+          <div className="profile-initials">
+            <img src={username.thumbnail} alt="Product Thumbnail" />
+          </div>
         </div>
         <div className="profile-info">
-            <h1>{username.name}</h1>
-            <p>{userInfo.email}</p>
-            <div className='content-profile-balance'>
-            <div className="profile-blance" >
-            <img className='coin-image-profile'
-                    src={coin}
-                      alt=""
-                      style={{ width: "38px", height: "35px", transform: "none", marginRight:"3px"}}
-                    />
-              <div className='coin-profile'>{userBl} 
-              </div>
-            </div>
+          <h1>{username.name}</h1>
+          <p>{userInfo.email}</p>
+          <div className="content-profile-balance">
+            <div className="profile-blance">
+              <img
+                className="coin-image-profile"
+                src={coin}
+                alt=""
+                style={{
+                  width: "38px",
+                  height: "35px",
+                  transform: "none",
+                  marginRight: "3px",
+                }}
+              />
+              <div className="coin-profile">{userBl}</div>
             </div>
           </div>
+        </div>
       </main>
 
       <div className="header-profile">
-      <div className="created-profile">
-        Đã tạo
-        </div>
-    </div>
-
-<div className='data-profile'>
-
-</div>
-<ul className="list-container-profile">
-  {data.map((list) => (
-    <li key={list.id} className="list-item-profile">
-      <div className='img-overflow'>
-      <div className="img-container-profile">
-        <img src={list.thumbnail} alt={list.name} />
+        <div className="created-profile">Đã tạo</div>
       </div>
-      </div>
-      
-      <div className="detail-container-profile">
-        <div className="left-column-profile">
-          <h3>{list.name}</h3>
-          <p style={{ width: "300px" }}></p>
-        </div>
-        
-      </div>
-      
-    </li>
-  ))}
-</ul>
 
-    
+      <div className="data-profile"></div>
+      <ul className="list-container-profile">
+        {data.map((list) => (
+          <li key={list.id} className="list-item-profile">
+            <div className="img-overflow">
+              <div className="img-container-profile">
+                <img src={list.thumbnail} alt={list.name} />
+              </div>
+            </div>
+
+            <div className="detail-container-profile">
+              <div className="left-column-profile">
+                <h3>{list.name}</h3>
+                <p style={{ width: "300px" }}></p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+
       <ToastContainer />
     </div>
-
-    
   );
-}
+};
 
 export default Profile;
