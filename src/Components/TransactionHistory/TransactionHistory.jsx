ce6ef230ba9318792d7f9f60f8f7bdbc7f230c92
khanhncd2008 <132860339+khanhncd2008@users.noreply.github.com>
2024-07-17 21:46:26 +0700

    addAuthForPage

--- src/Components/TransactionHistory/TransactionHistory.jsx
+++ src/Components/TransactionHistory/TransactionHistory.jsx
@@ -1,97 +1,131 @@
-import React, { useState, useEffect } from "react";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
-import axios from "../../utils/axios.js";
-import { jwtDecode } from "jwt-decode";
-import { toast, ToastContainer } from "react-toastify";
-import { useParams } from "react-router-dom";
-import { Table } from 'antd';
-import "./TransactionHistory.css";
+import React, {useState, useEffect} from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import axios from '../../utils/axios.js';
+import {jwtDecode} from 'jwt-decode';
+import {toast, ToastContainer} from 'react-toastify';
+import {useParams} from 'react-router-dom';
+import {Table} from 'antd';
+import {useLocation, useNavigate} from 'react-router-dom';
+import './TransactionHistory.css';
 
 const columns = [
-  {
-    title: 'Transaction Type',
-    dataIndex: 'transactionType',
-    key: 'transactionType',
-    width: 150,
-  },
-  {
-    title: 'Date',
-    dataIndex: 'date',
-    key: 'date',
-    width: 150,
-  },
-  {
-    title: 'Time',
-    dataIndex: 'time',
-    key: 'time',
-    width: 150,
-  },
-  {
-    title: 'Amount',
-    dataIndex: 'amount',
-    key: 'amount',
-    width: 150,
-  },
+	{
+		title: 'Transaction Type',
+		dataIndex: 'transactionType',
+		key: 'transactionType',
+		width: 150,
+	},
+	{
+		title: 'Date',
+		dataIndex: 'date',
+		key: 'date',
+		width: 150,
+	},
+	{
+		title: 'Time',
+		dataIndex: 'time',
+		key: 'time',
+		width: 150,
+	},
+	{
+		title: 'Amount',
+		dataIndex: 'amount',
+		key: 'amount',
+		width: 150,
+	},
 ];
 
 export const TransactionHistory = () => {
-  const { id } = useParams();
-  const [data, setData] = useState([]);
+	const {id} = useParams();
+	const [data, setData] = useState([]);
+	const [isAuthorized, setIsAuthorized] = useState(null); // null for loading state
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
+	useEffect(() => {
+		const checkUserScreenAccess = async () => {
+			try {
+				const response = await axios.post('Auth/screen', {
+					resource: 'UserScreen',
+				});
+				if (response.data.isSuccess) {
+					setIsAuthorized(true);
+				} else {
+					setIsAuthorized(false);
+				}
+			} catch (error) {
+				console.error('Error checking user screen access:', error);
+				setIsAuthorized(false);
+			}
+		};
 
-    const fetchData = async () => {
-      try {
-        const token = localStorage.getItem("accessToken");
-        if (token) {
-          const decoded = jwtDecode(token);
-          const userId = decoded.nameid;
-          console.log(userId);
-          await axios.get(`Account/user/${userId}`);
+		checkUserScreenAccess();
+	}, []);
 
-          const purchaseTicket = await axios.get(`TransactionHistory/getListTransactionHistoryByUserId/10/1`);
-          const formattedData = purchaseTicket.data.value.map(transaction => {
-            const date = new Date(transaction.createdOn);
-            return {
-              key: transaction.id,
-              transactionType: transaction.transactionType,
-              date: date.toLocaleDateString(),
-              time: date.toLocaleTimeString(),
-              amount: transaction.amount,
-            };
-          });
-          setData(formattedData);
-        } else {
-          toast.error("Bạn chưa đăng nhập");
-          window.location.href = "http://localhost:3000/homepage";
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+	useEffect(() => {
+		if (isAuthorized === false) {
+			navigate('/login');
+		}
+	}, [isAuthorized, navigate]);
 
-    fetchData();
-  }, [id]);
+	useEffect(() => {
+		window.scrollTo(0, 0);
 
-  return (
-    <div className="transactionHistorypage">
-      <Header />
-      <div className="transactionHistorypage-content">
-        <h2>Lịch sử giao dịch</h2>
-        <div className="transactionHistorypage-form">
-          <Table
-            columns={columns}
-            dataSource={data}
-            scroll={{ x: 1000, y: 300 }}
-          />
-        </div>
-      </div>
-      <Footer />
-      <ToastContainer />
-    </div>
-  );
+		const fetchData = async () => {
+			try {
+				const token = localStorage.getItem('accessToken');
+				if (token) {
+					const decoded = jwtDecode(token);
+					const userId = decoded.nameid;
+					console.log(userId);
+					await axios.get(`Account/user/${userId}`);
+
+					const purchaseTicket = await axios.get(
+						`TransactionHistory/getListTransactionHistoryByUserId/10/1`
+					);
+					const formattedData = purchaseTicket.data.value.map((transaction) => {
+						const date = new Date(transaction.createdOn);
+						return {
+							key: transaction.id,
+							transactionType: transaction.transactionType,
+							date: date.toLocaleDateString(),
+							time: date.toLocaleTimeString(),
+							amount: transaction.amount,
+						};
+					});
+					setData(formattedData);
+				}
+			} catch (error) {
+				console.error('Error fetching data:', error);
+			}
+		};
+
+		fetchData();
+	}, [id]);
+
+	if (!isAuthorized) {
+		return (
+			<div>
+				<div className="loading-container">
+					<div className="loading-spinner"></div>
+				</div>
+			</div>
+		);
+	} else {
+		return (
+			<div className="transactionHistorypage">
+				<Header />
+				<div className="transactionHistorypage-content">
+					<h2>Lịch sử giao dịch</h2>
+					<div className="transactionHistorypage-form">
+						<Table columns={columns} dataSource={data} scroll={{x: 1000, y: 300}} />
+					</div>
+				</div>
+				<Footer />
+				<ToastContainer />
+			</div>
+		);
+	}
 };
 
 export default TransactionHistory;
