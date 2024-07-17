e138a4096f1519529385450e9e318dfda4b3d928
khanhncd2008 <132860339+khanhncd2008@users.noreply.github.com>
2024-07-17 20:51:46 +0700

    addAuthForScreen

--- src/Components/ProductByCategory/ProductByCategory.jsx
+++ src/Components/ProductByCategory/ProductByCategory.jsx
@@ -5,11 +5,40 @@ import axios from '../../utils/axios.js';
 import {useParams} from 'react-router-dom';
 import coin from '../Assets/coin.png';
 import {Link} from 'react-router-dom';
+import {useLocation, useNavigate} from 'react-router-dom';
 import './ProductByCategory.css';
 
 export const ProductByCategory = () => {
 	const [searchResults, setSearchResults] = useState([]);
 	const {id} = useParams();
+	const [isAuthorized, setIsAuthorized] = useState(null); // null for loading state
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const checkUserScreenAccess = async () => {
+			try {
+				const response = await axios.post('Auth/screen', {
+					resource: 'UserScreen',
+				});
+				if (response.data.isSuccess) {
+					setIsAuthorized(true);
+				} else {
+					setIsAuthorized(false);
+				}
+			} catch (error) {
+				console.error('Error checking user screen access:', error);
+				setIsAuthorized(false);
+			}
+		};
+
+		checkUserScreenAccess();
+	}, []);
+
+	useEffect(() => {
+		if (isAuthorized === false) {
+			navigate('/login');
+		}
+	}, [isAuthorized, navigate]);
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -24,68 +53,80 @@ export const ProductByCategory = () => {
 		fetchData();
 	}, [id]);
 
-	return (
-		<div>
-			<Header />
-			<div className="search-results">
-				{searchResults.length === 0 ? (
-					<p className="title-notfound-category">
-						Không tìm thấy sản phẩm nào khớp với lựa chọn của bạn.
-					</p>
-				) : (
-					<div className="data-list">
-						<ul className="list-container" style={{marginTop: '65px'}}>
-							{searchResults.map((list) => (
-								<li key={list.id} className="list-item">
-									<div className="img-container">
-										<img src={list.thumbnail} alt={list.name} />
-									</div>
-									<Link
-										to={`/productdetail/${list.id}`}
-										style={{textDecoration: 'none'}}
-									>
-										<div className="detail-container">
-											<div className="left-column">
-												<h3>{list.name}</h3>
-												<p style={{width: '200px'}}>{list.description}</p>
-											</div>
-											<div className="right-column">
-												<p
-													style={{
-														display: 'flex',
-														justifyContent: 'flex-end',
-														alignItems: 'center',
-													}}
-												>
-													<img
-														src={coin}
-														alt=""
-														style={{
-															width: '38px',
-															height: '35px',
-															transform: 'none',
-															marginRight: '3px',
-														}}
-													/>
-													<p>{list.price}</p>
-												</p>
-											</div>
-										</div>
-									</Link>
-									<div style={{textAlign: 'center'}}>
-										<Link to={`/orderproduct/${list.id}`}>
-											<button className="buy-button">Mua hàng</button>
-										</Link>
-									</div>
-								</li>
-							))}
-						</ul>
-					</div>
-				)}
+	if (!isAuthorized) {
+		return (
+			<div>
+				<div className="loading-container">
+					<div className="loading-spinner"></div>
+				</div>
 			</div>
-			<Footer />
-		</div>
-	);
+		);
+	} else {
+		return (
+			<div>
+				<Header />
+				<div className="search-results">
+					{searchResults.length === 0 ? (
+						<p className="title-notfound-category">
+							Không tìm thấy sản phẩm nào khớp với lựa chọn của bạn.
+						</p>
+					) : (
+						<div className="data-list">
+							<ul className="list-container" style={{marginTop: '65px'}}>
+								{searchResults.map((list) => (
+									<li key={list.id} className="list-item">
+										<div className="img-container">
+											<img src={list.thumbnail} alt={list.name} />
+										</div>
+										<Link
+											to={`/productdetail/${list.id}`}
+											style={{textDecoration: 'none'}}
+										>
+											<div className="detail-container">
+												<div className="left-column">
+													<h3>{list.name}</h3>
+													<p style={{width: '200px'}}>
+														{list.description}
+													</p>
+												</div>
+												<div className="right-column">
+													<p
+														style={{
+															display: 'flex',
+															justifyContent: 'flex-end',
+															alignItems: 'center',
+														}}
+													>
+														<img
+															src={coin}
+															alt=""
+															style={{
+																width: '38px',
+																height: '35px',
+																transform: 'none',
+																marginRight: '3px',
+															}}
+														/>
+														<p>{list.price}</p>
+													</p>
+												</div>
+											</div>
+										</Link>
+										<div style={{textAlign: 'center'}}>
+											<Link to={`/orderproduct/${list.id}`}>
+												<button className="buy-button">Mua hàng</button>
+											</Link>
+										</div>
+									</li>
+								))}
+							</ul>
+						</div>
+					)}
+				</div>
+				<Footer />
+			</div>
+		);
+	}
 };
 
 export default ProductByCategory;
